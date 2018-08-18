db61fdbfcf7ecce33758e1c7169ceb191c95e5c2
Alexander Karpov <alexander.karpoff@gmail.com>
2018-08-18 08:30:14 +0300

    jpa

---
+++ src/main/java/com/example/spring5webapp/model/Author.java
@@ -0,0 +1,20 @@
+package com.example.spring5webapp.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+}
